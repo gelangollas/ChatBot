c6bf677c731440d20f0b81e188dea96ae691a797
gelangollas <gelangollas@gmail.com>
2017-03-30 00:25:26 +0700

    RELEASEgit pushgit push

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,5 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        ChatBot bot = new ChatBot();
+        while(true){
+            System.out.print(bot.GetResponce() + '\n');
+            String request = System.console().readLine();
+            bot.Ask(request);
+        }
     }
 }
